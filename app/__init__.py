a792ba54fa803d3c0b69d1c699734d44f4704869
Leandro Del Arco <leandrodelarco@outlook.com>
2023-05-22 16:37:42 -0300

    Add home blueprint and template

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,19 @@
+import os
+
 from flask import Flask
 
-app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return { "message": "hello" }
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(SECRET_KEY='dev')
+
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        app.config.from_mapping(test_config)
+
+    from . import home
+    app.register_blueprint(home.bp)
+    app.add_url_rule('/', endpoint='index')
+
+    return app
