bb287cb22e477f2f61f9f604aae933e1eb11d446
Leandro Del Arco <leandrodelarco@outlook.com>
2023-05-23 13:05:01 -0300

    Refactor app class and implement pieces-list operations

--- app/static/app.js
+++ app/static/app.js
@@ -1,42 +1,72 @@
-var app;
+'use strict';
 
-(function (app) {
-    'use strict';
+class App {
 
     /**
      * Initialize DOM elements references.
      */
-    app.initialize = function () {
+    initialize() {
 
         this.pieces = [];
         this.piecesListUL = document.querySelector("#pieces-list");
         this.pieceTemplate = document.querySelector("#piece-template");
-    };
+    }
 
     /**
      * Clear pieces list.
      */
-    app.clearPieces = function() {
+    clearPieces() {
 
-        // TODO: implement
-    };
+        this.pieces = [];
+        this.piecesListUL.innerHTML = '';
+    }
 
     /**
      * Add piece to list.
      * @param {*} piece 
      */
-    app.addPiece = function(piece) {
+    addPiece(piece) {
 
-        // TODO: implement
-    };
+        piece.id = this.pieces.length + 1;
+        this.pieces.push(piece);
+
+        // add new LI element
+        const newPiece = this.pieceTemplate.content.cloneNode(true);
+        newPiece.querySelector("li").setAttribute("piece-id", piece.id);
+        newPiece.querySelector(".piece-id").innerText = `#${piece.id}`;
+        newPiece.querySelector("label").innerText = `${piece.width}mm x ${piece.height}mm`;
+        newPiece.querySelector(".remove-button").addEventListener("click", () => this.removePiece(piece.id));
+        this.piecesListUL.appendChild(newPiece);
+    }
 
     /**
      * Remove piece from list.
      * @param {*} pieceId 
      */
-    app.removePiece = function(pieceId) {
+    removePiece(pieceId) {
 
-        // TODO: implement
-    };
+        // remove piece from list
+        this.pieces = this.pieces.filter(piece => piece.id != pieceId);
 
-})(app || (app = {}));
+        // update pieces id
+        this.pieces.filter(piece => piece.id > pieceId).forEach(piece => piece.id--);
+
+        // remove element
+        const pieceElement = this.piecesListUL.querySelector(`li[piece-id="${pieceId}"]`);
+        this.piecesListUL.removeChild(pieceElement);
+
+        // update elements id
+        Array.from(this.piecesListUL.querySelectorAll("li")).forEach(li => {
+
+            const id = li.getAttribute("piece-id");
+
+            if(id > pieceId) {
+                li.setAttribute("piece-id", id - 1);
+                li.querySelector(".piece-id").innerText = `#${id - 1}`;
+            }
+        });
+    }
+
+}
+
+var app = new App();
